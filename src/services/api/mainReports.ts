9a8c8669ac910925d5a458d6f73f3f2e4c737b81
Abdulloev Buzurgmehr <thesarboz@gmail.com>
2024-11-01 12:22:27 +0200

    Update 1.6.2: Bug fix

--- src/services/api/mainReports.ts
+++ src/services/api/mainReports.ts
@@ -1,5 +1,4 @@
 import fileDownload from "js-file-download"
-import IAPIResposeFormat from "./IAPIResposeFormat"
 import axiosClient from "./axiosClient"
 
 const URL = "/main-reports"
@@ -17,15 +16,6 @@ export async function projectProgressReport(): Promise<boolean> {
 export async function analysisOfRemainingMaterials(): Promise<boolean> {
   const responseRaw = await axiosClient.get(`${URL}/analysis-of-remaining-materials`, { responseType: "blob", })
   if (responseRaw.status == 200) {
-    if (typeof responseRaw.data == 'object') {
-      const response: IAPIResposeFormat<null> = responseRaw.data
-      if (response.success && response.permission) {
-        return true
-      } else {
-        throw new Error(response.error)
-      }
-    }
-
     fileDownload(responseRaw.data, "Аналитика Остатка Материалов.xlsx")
     return true
   } else {
